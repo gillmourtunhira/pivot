e05ca3f91ecf9e4710ac47557bd3eab7b634f172
Gillmour <gillmourtunhira@gmail.com>
2022-11-08 18:48:30 +0200

    disable plugin functions

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@ function pivot_login_logo(){
             text-align: center;
             font-size: 28px;
             text-transform: uppercase;
-            font-weight: 200;
+            font-weight: 700;
         }
     </style>
 <?php
@@ -109,3 +109,49 @@ function pivot_remove_support(){
 	// remove_post_type_support('page', 'editor');
 }
 add_action( 'init', 'pivot_remove_support' );
+
+// Removing plugin controls from admin
+function remove_plugin_controls($actions, $plugin_file, $plugin_data, $content){
+
+    if( array_key_exists('edit', $actions) ){
+        unset($actions['edit']);
+    }
+
+    if( array_key_exists('deactivate', $actions) ){
+        unset($actions['deactivate']);
+    }
+
+    if( array_key_exists('activate',$actions) ){
+        unset($actions['activate']);
+    }
+
+    if( array_key_exists('delete',$actions) ){
+        unset($actions['delete']);
+    }
+
+    return $actions;
+
+}
+add_filter('plugin_action_links', 'remove_plugin_controls', 10, 4);
+
+// Remove bulk action options for managing plugins
+function disable_bulk_actions($actions){
+
+    if( array_key_exists('deactivate-selected', $actions) ){
+        unset( $actions['deactivate-selected'] );
+    }
+
+    if( array_key_exists('activate-selected', $actions) ){
+        unset( $actions['activate-selected'] );
+    }
+
+    if( array_key_exists('delete-selected', $actions) ){
+        unset( $actions['delete-selected'] );
+    }
+
+    if( array_key_exists('update-selected', $actions) ){
+        unset( $actions['update-selected'] );
+    }
+
+}
+add_filter('bulk_actions-plugins', 'disable_bulk_actions');
